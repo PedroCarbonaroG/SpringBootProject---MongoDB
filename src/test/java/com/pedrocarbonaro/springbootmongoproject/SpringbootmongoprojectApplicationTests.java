f7e32afdf5c083dcf66d877b4eb045887016cd2e
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-04-09 21:26:17 -0300

    First commit - Starting Project!

---
+++ src/test/java/com/pedrocarbonaro/springbootmongoproject/SpringbootmongoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrocarbonaro.springbootmongoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
