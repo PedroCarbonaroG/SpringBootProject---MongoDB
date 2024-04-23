34d17a42df04fc5c73d646441aab3d1b0fe22d16
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-04-23 09:24:05 -0300

    feat: Included comments to posts

--- src/main/java/com/pedrocarbonaro/springbootmongoproject/config/Instantiation.java
+++ src/main/java/com/pedrocarbonaro/springbootmongoproject/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.pedrocarbonaro.springbootmongoproject.config;
 
 import com.pedrocarbonaro.springbootmongoproject.domain.Post;
 import com.pedrocarbonaro.springbootmongoproject.dto.AuthorDTO;
+import com.pedrocarbonaro.springbootmongoproject.dto.CommentDTO;
 import com.pedrocarbonaro.springbootmongoproject.repository.PostRepository;
 import com.pedrocarbonaro.springbootmongoproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TimeZone;
 
 @Configuration
@@ -40,7 +42,14 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Let's trip!", "I'm coming to São Paulo, hugs!!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good morning!", "i woke up happy today.", new AuthorDTO(maria));
-        
+
+        CommentDTO c1 = new CommentDTO("Good trip bro!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Enjoy the trip!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Have a nice day!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
--- src/main/java/com/pedrocarbonaro/springbootmongoproject/domain/Post.java
+++ src/main/java/com/pedrocarbonaro/springbootmongoproject/domain/Post.java
@@ -1,11 +1,14 @@
 package com.pedrocarbonaro.springbootmongoproject.domain;
 
 import com.pedrocarbonaro.springbootmongoproject.dto.AuthorDTO;
+import com.pedrocarbonaro.springbootmongoproject.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -18,6 +21,8 @@ public class Post implements Serializable {
     private String body;
 
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
@@ -65,6 +70,10 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 
     @Override
