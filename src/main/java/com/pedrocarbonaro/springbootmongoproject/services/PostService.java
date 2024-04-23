36e1632c4950f5ef21f382e164d795469f20dd8a
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-04-23 09:16:07 -0300

    feat: Getting post by Id

---
+++ src/main/java/com/pedrocarbonaro/springbootmongoproject/services/PostService.java
@@ -0,0 +1,21 @@
+package com.pedrocarbonaro.springbootmongoproject.services;
+
+import com.pedrocarbonaro.springbootmongoproject.domain.Post;
+import com.pedrocarbonaro.springbootmongoproject.repository.PostRepository;
+import com.pedrocarbonaro.springbootmongoproject.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Object id wasn't found!"));
+    }
+}
